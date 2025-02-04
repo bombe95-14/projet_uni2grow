28eaf70039b0846df0aa88ff2f5e53b840fd2ece
bombe95-14 <kenfackkeng1995@gmail.com>
2025-02-04 11:12:08 +0100

    push du 04/02/2025 controller sur les entités address et customer achevé

--- backendprojet/src/main/java/com/example/spring_web/backendprojet/controller/CustomerController.java
+++ backendprojet/src/main/java/com/example/spring_web/backendprojet/controller/CustomerController.java
@@ -3,7 +3,9 @@ package com.example.spring_web.backendprojet.controller;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -12,6 +14,7 @@ import org.springframework.web.bind.annotation.RestController;
 import com.example.spring_web.backendprojet.dto.bodyrequest.AddressFilter;
 import com.example.spring_web.backendprojet.dto.bodyrequest.CreateAndUpdateAddressDto;
 import com.example.spring_web.backendprojet.dto.bodyrequest.CreateAndUpdateCustomerDto;
+import com.example.spring_web.backendprojet.dto.bodyrequest.CustomerFilter;
 import com.example.spring_web.backendprojet.traitement.CustomerTraitement;
 
 @RequestMapping("customer")
@@ -32,13 +35,23 @@ public class CustomerController {
     }
 
     @PostMapping("/filter")
-    public ResponseEntity<?> getAllAddressByFiltering( @RequestBody AddressFilter addressFilter) {
+    public ResponseEntity<?> getAllAddressByFiltering( @RequestBody CustomerFilter customerFilter ) {
         return ResponseEntity.ok("Address");
     }
 
-      @PostMapping("/add")
+    @PostMapping("/add")
     public ResponseEntity<?> createOneCustomer( @RequestBody CreateAndUpdateCustomerDto createAndUpdateCustomerDto) {
         return customerTraitement.createCustomer(createAndUpdateCustomerDto);
+    }
+
+    @PutMapping("/update")
+    public ResponseEntity<?> updateOneCustomer( @RequestBody CreateAndUpdateCustomerDto createAndUpdateCustomerDto) {
+        return customerTraitement.updateOneCustomer(createAndUpdateCustomerDto);
+    }
+
+    @PostMapping("/delete/{idCustomer}")
+    public ResponseEntity<?> deleteOneCustomer( @PathVariable("idCustomer") Long idCustomer) {
+        return customerTraitement.deleteOneCustomer(idCustomer);
     }
 
 }
--- backendprojet/src/main/java/com/example/spring_web/backendprojet/traitement/CustomerTraitement.java
+++ backendprojet/src/main/java/com/example/spring_web/backendprojet/traitement/CustomerTraitement.java
@@ -40,6 +40,15 @@ public class CustomerTraitement {
                 return ResponseEntity.badRequest().body("Empty obligatories field");
         }
 
+        if ( customerService.findByPhone( createAndUpdateCustomerDto.getPhone() ).isPresent() ) {
+            return ResponseEntity.badRequest().body("The " + createAndUpdateCustomerDto.getPhone() + " telephone number is already being used by a customer");
+        }
+
+        if ( ( Objects.isNull(createAndUpdateCustomerDto.getEmail() ) || createAndUpdateCustomerDto.getEmail().isEmpty() || createAndUpdateCustomerDto.getEmail().isBlank() ) && 
+            customerService.findByEmail( createAndUpdateCustomerDto.getEmail().trim() ).isPresent() ) {
+                return ResponseEntity.badRequest().body( "The " + createAndUpdateCustomerDto.getEmail() + " email address is already used by a customer");
+        }
+       
         Optional<Address> addressOptional =  addressService.findByZipCode( createAndUpdateCustomerDto.getCreateAndUpdateAddressDto().getZipCode().trim() );
         Address address = null;
         if ( addressOptional.isPresent() ) {
@@ -86,42 +95,85 @@ public class CustomerTraitement {
 
         Optional<Customer> customerOptional = customerService.findById(createAndUpdateCustomerDto.getIdCustomer());
         if ( customerOptional.isEmpty() ) {
-            return ResponseEntity.badRequest().body("Empty obligatories field");
+            return ResponseEntity.badRequest().body("Customer not found");
         }
 
-        Optional<Address> addressOptional =  addressService.findByZipCode( createAndUpdateCustomerDto.getCreateAndUpdateAddressDto().getZipCode().trim() );
-        Address address = null;
-        if ( addressOptional.isPresent() ) {
-            address = addressOptional.get();
-        } else {
-
-            CreateAndUpdateAddressDto createAndUpdateAddressDto = createAndUpdateCustomerDto.getCreateAndUpdateAddressDto();
+        Address address = customerOptional.get().getAddress();
+        if ( !address.getZipCode().equalsIgnoreCase( createAndUpdateCustomerDto.getCreateAndUpdateAddressDto().getZipCode() ) ) {
             
-            if ( Objects.isNull(createAndUpdateAddressDto.getCity() ) || createAndUpdateAddressDto.getCity().isEmpty() || createAndUpdateAddressDto.getCity().isBlank() || 
-            Objects.isNull( createAndUpdateAddressDto.getCountry()) || createAndUpdateAddressDto.getCountry().isEmpty() || createAndUpdateAddressDto.getCountry().isBlank() || 
-             Objects.isNull(createAndUpdateAddressDto.getStreet())  || createAndUpdateAddressDto.getStreet().isEmpty() || createAndUpdateAddressDto.getStreet().isBlank() || 
-             Objects.isNull(createAndUpdateAddressDto.getState() ) || createAndUpdateAddressDto.getState().isEmpty() || createAndUpdateAddressDto.getState().isBlank() ){
-                 return ResponseEntity.badRequest().body("Empty obligatories field");
+            Optional<Address> addressOptional =  addressService.findByZipCode( createAndUpdateCustomerDto.getCreateAndUpdateAddressDto().getZipCode().trim() );
+            if ( addressOptional.isPresent() ) {
+                address = addressOptional.get();
+            } else {
+
+                CreateAndUpdateAddressDto createAndUpdateAddressDto = createAndUpdateCustomerDto.getCreateAndUpdateAddressDto();
+                
+                if ( Objects.isNull(createAndUpdateAddressDto.getCity() ) || createAndUpdateAddressDto.getCity().isEmpty() || createAndUpdateAddressDto.getCity().isBlank() || 
+                Objects.isNull( createAndUpdateAddressDto.getCountry()) || createAndUpdateAddressDto.getCountry().isEmpty() || createAndUpdateAddressDto.getCountry().isBlank() || 
+                Objects.isNull(createAndUpdateAddressDto.getStreet())  || createAndUpdateAddressDto.getStreet().isEmpty() || createAndUpdateAddressDto.getStreet().isBlank() || 
+                Objects.isNull(createAndUpdateAddressDto.getState() ) || createAndUpdateAddressDto.getState().isEmpty() || createAndUpdateAddressDto.getState().isBlank() ){
+                    return ResponseEntity.badRequest().body("Empty obligatories field");
+                }
+    
+            Address addressSave = new Address(); 
+            addressSave.setCity( createAndUpdateAddressDto.getCity().trim() );
+            addressSave.setCountry( createAndUpdateAddressDto.getCountry().trim() );
+            addressSave.setStreet( createAndUpdateAddressDto.getStreet().trim() );
+            addressSave.setState( createAndUpdateAddressDto.getState().trim() );
+            addressSave.setZipCode( createAndUpdateAddressDto.getZipCode().trim() );
+    
+            address = addressService.save( addressSave ); 
             }
- 
-        Address addressSave = new Address(); 
-        addressSave.setCity( createAndUpdateAddressDto.getCity().trim() );
-        addressSave.setCountry( createAndUpdateAddressDto.getCountry().trim() );
-        addressSave.setStreet( createAndUpdateAddressDto.getStreet().trim() );
-        addressSave.setState( createAndUpdateAddressDto.getState().trim() );
-        addressSave.setZipCode( createAndUpdateAddressDto.getZipCode().trim() );
- 
-         address = addressService.save( addressSave ); 
+
+        }     
+
+        if ( customerOptional.get().getPhone()!=null && !customerOptional.get().getPhone().isEmpty() && !createAndUpdateCustomerDto.getPhone().isBlank() ) {
+            
+            if ( !customerOptional.get().getPhone().equals( createAndUpdateCustomerDto.getPhone().trim() ) ) {
+                if ( customerService.findByPhone( createAndUpdateCustomerDto.getPhone().trim() ).isPresent() ) {
+                    return ResponseEntity.badRequest().body("The " + createAndUpdateCustomerDto.getPhone() + " telephone number is already being used by a customer");
+                }
+            }
+
+        } 
+
+        if (  !( Objects.isNull(createAndUpdateCustomerDto.getEmail() ) || createAndUpdateCustomerDto.getEmail().isEmpty() || createAndUpdateCustomerDto.getEmail().isBlank() ) ){
+        
+            if ( Objects.nonNull(customerOptional.get().getEmail()) && !customerOptional.get().getEmail().isEmpty()  ) {
+                if ( !customerOptional.get().getEmail().equalsIgnoreCase( createAndUpdateCustomerDto.getEmail() ) && customerService.findByEmail( createAndUpdateCustomerDto.getEmail().trim() ).isPresent()  ) {
+                    return ResponseEntity.badRequest().body( "The " + createAndUpdateCustomerDto.getEmail() + " email address is already used by a customer");
+                }
+            } else if ( customerService.findByEmail( createAndUpdateCustomerDto.getEmail().trim() ).isPresent()  ) {
+                return ResponseEntity.badRequest().body( "The " + createAndUpdateCustomerDto.getEmail() + " email address is already used by a customer");
+            }
+
         }
 
-        Customer customer = new Customer();
-        customer.setAddress(address);
-        customer.setEmail( createAndUpdateCustomerDto.getEmail() );
-        customer.setName( createAndUpdateCustomerDto.getName() );
-        customer.setPhone( createAndUpdateCustomerDto.getPhone() );
+        if (  !( Objects.isNull(createAndUpdateCustomerDto.getEmail() ) || createAndUpdateCustomerDto.getEmail().isEmpty() || createAndUpdateCustomerDto.getEmail().isBlank() ) && 
+            Objects.nonNull(customerOptional.get().getEmail()) && !customerOptional.get().getEmail().isEmpty() && !customerOptional.get().getEmail().equalsIgnoreCase( createAndUpdateCustomerDto.getEmail() ) &&
+            customerService.findByEmail( createAndUpdateCustomerDto.getEmail().trim() ).isPresent() ) {
+                return ResponseEntity.badRequest().body( "The " + createAndUpdateCustomerDto.getEmail() + " email address is already used by a customer");
+        }
+        
+        customerOptional.get().setAddress(address);
+        customerOptional.get().setEmail( createAndUpdateCustomerDto.getEmail() );
+        customerOptional.get().setName( createAndUpdateCustomerDto.getName() );
+        customerOptional.get().setPhone( createAndUpdateCustomerDto.getPhone() );
 
-        Customer customerSave = customerService.save(customer);
+        Customer customerSave = customerService.save(customerOptional.get());
         return ResponseEntity.ok( customerSave );
     }
 
+    public ResponseEntity<?> deleteOneCustomer( Long idCustomer ){
+
+        Optional <Customer> customerOptional = customerService.findById( idCustomer );
+        if ( customerOptional.isEmpty() ) {
+            return ResponseEntity.badRequest().body("Customer not found");
+        }
+
+        customerService.delete(customerOptional.get());
+
+        return ResponseEntity.ok( " this customer has been deleted " );
+
+    }
 }
